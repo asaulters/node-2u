234e821e23b84a4a5e70d8f2113477c8818ee593
Adam Saulters <adamsaulters@gmail.com>
2021-08-23 21:00:50 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views"); //views directory
+
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); //css
+
+app.use("/admin", adminData.routes);
+app.use(shopRoutes);
+
+app.use("/", (req, res, next) => {
+  res.status(404).render("404", { pageTitle: "Page Not Found" });
+});
+
+app.listen(3000);
+console.log("listening on port 3000");
